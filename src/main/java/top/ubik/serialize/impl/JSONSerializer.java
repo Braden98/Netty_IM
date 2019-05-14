8186c14436d2f5a7e97b7d20f557d4201b0922e3
Ubique0305 <1071763478@qq.com>
2019-05-14 14:17:02 +0800

    增加 序列化协议部分

---
+++ src/main/java/top/ubik/serialize/impl/JSONSerializer.java
@@ -0,0 +1,25 @@
+package top.ubik.serialize.impl;
+
+import com.alibaba.fastjson.JSON;
+import top.ubik.serialize.Serializer;
+import top.ubik.serialize.SerializerAlgorithm;
+
+public class JSONSerializer implements Serializer {
+
+    @Override
+    public byte getSerializerAlgorithm() {
+        return SerializerAlgorithm.JSON;
+    }
+
+    @Override
+    public byte[] serialize(Object object) {
+
+        return JSON.toJSONBytes(object);
+    }
+
+    @Override
+    public <T> T deserialize(Class<T> clazz, byte[] bytes) {
+
+        return JSON.parseObject(bytes, clazz);
+    }
+}
