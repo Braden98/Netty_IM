ac67c57924b92fd57f321cc1a1ca1f7c3eab6a59
Ubique0305 <1071763478@qq.com>
2019-05-14 19:14:41 +0800

    增加了编解码器和拆包器

---
+++ src/main/java/top/ubik/codec/PacketEncoder.java
@@ -0,0 +1,15 @@
+package top.ubik.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToByteEncoder;
+import top.ubik.protocol.Packet;
+import top.ubik.protocol.PacketCodec;
+
+public class PacketEncoder extends MessageToByteEncoder<Packet> {
+
+    @Override
+    protected void encode(ChannelHandlerContext ctx, Packet packet, ByteBuf out) {
+        PacketCodec.INSTANCE.encode(out, packet);
+    }
+}
